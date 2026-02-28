203ef437a9bd3c2b058f81bf5a4ed7b47deed6bc
Kushagra077 <p.kushagra22@gmail.com>
2026-02-28 09:42:52 +0530

    Fix method name in JournalManager and add JSON data file for sessions

--- data_module.py
+++ data_module.py
@@ -1,3 +1,4 @@
+import json
 from pydantic import BaseModel, Field
 from datetime import date
 
@@ -9,11 +10,18 @@ class CodingSession(BaseModel):
 class JournalManager(BaseModel):
         sessions : list[CodingSession]  = Field(default_factory=list)
 
-        def add_sessions(self, new_session : CodingSession):
+        def add_session(self, new_session : CodingSession):
             self.sessions.append(new_session)
-                
+
+        def save_to_file(self, filename : str = 'journal.json'):
+            data_to_save = [session.model_dump(mode="json") for session in self.sessions]
+            with open(filename, 'w') as f:
+                  json.dump(data_to_save , f, indent=4)
+
+
 if __name__ == "__main__":
        manager = JournalManager()
        session1 = CodingSession(date = date.today() , duration_minutes = 120, energy_level = 10)
-       manager.add_sessions(session1)
+       manager.add_session(session1)
        print(manager.sessions)
+       manager.save_to_file()
