e80e2a3f44e9f8742718011fe5463e87b5350a43
Kushagra077 <p.kushagra22@gmail.com>
2026-03-02 09:07:01 +0530

    Refactor JournalManager to remove Pydantic dependency and streamline file handling

--- manager.py
+++ manager.py
@@ -1,21 +1,26 @@
 import json
-from pydantic import BaseModel, Field
 from models import CodingSession
 
-class JournalManager(BaseModel):
-        sessions : list[CodingSession]  = Field(default_factory=list)
+class JournalManager():
+        
+        def __init__(self, filename : str = 'journal.json'):
+             
+            self.filename = filename
+            self.sessions : list[CodingSession] = []
+            self.load_from_file()
 
         def add_session(self, new_session : CodingSession):
             self.sessions.append(new_session)
+            self.save_to_file()
 
-        def save_to_file(self, filename : str = 'journal.json'):
+        def save_to_file(self):
             data_to_save = [session.model_dump(mode="json") for session in self.sessions]
-            with open(filename, 'w') as f:
+            with open(self.filename, 'w') as f:
                   json.dump(data_to_save , f, indent=4)
 
-        def load_from_file(self, filename : str = 'journal.json'):
+        def load_from_file(self):
             try:
-                with open(filename, 'r') as f:
+                with open(self.filename, 'r') as f:
                     raw_data = json.load(f)
                     self.sessions = [CodingSession(**item) for item in raw_data]
             except FileNotFoundError:
