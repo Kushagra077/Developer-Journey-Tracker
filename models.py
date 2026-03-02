c5f149cc3dee4f5e3cb0784957aa9cd47263f078
Kushagra077 <p.kushagra22@gmail.com>
2026-03-02 09:07:08 +0530

    Fix date import in CodingSession model to use alias for clarity

--- models.py
+++ models.py
@@ -1,9 +1,9 @@
 import json
 from pydantic import BaseModel, Field
-from datetime import date
+from datetime import date as dt_date
 
 class CodingSession(BaseModel):
-        date : date
+        date: dt_date = Field(default_factory=dt_date.today)
         duration_minutes : int = Field(gt=0)
         energy_level : int = Field(ge=1, le=10)
 
